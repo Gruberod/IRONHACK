c272ab1e21bdd125a4dbc774b56171900f6a68af
Gruberova, Daniela <daniela.gruberova@merck.com>
2016-08-10 17:46:16 +0200

    rover kata initial commit

--- JavaScript/mars_rover/rover.js
+++ JavaScript/mars_rover/rover.js
@@ -22,4 +22,57 @@ function goForward(rover) {
   console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
 }
 
+function turnLeft(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'W'
+      break;
+    case 'E':
+      rover.direction = 'N'
+      break;
+    case 'S':
+      rover.direction = 'E'
+      break;
+    case 'W':
+      rover.direction = 'S'
+      break;
+  };
+}
+
+function turnRight(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'E'
+      break;
+    case 'E':
+      rover.direction = 'S'
+      break;
+    case 'S':
+      rover.direction = 'W'
+      break;
+    case 'W':
+      rover.direction = 'N'
+      break;
+  };
+}
+
+function goBackwards(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.position[0]--
+      break;
+    case 'E':
+      rover.position[1]--
+      break;
+    case 'S':
+      rover.position[0]++
+      break;
+    case 'W':
+      rover.position[1]++
+      break;
+  };
+
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+}
+
 goForward(myRover);
